585cdfc015f6ddd1fcb0df311249af6b97ee3f6b
JoelhansenMN <joelhansen22@gmail.com>
2024-06-04 12:58:15 -0500

    footer, header, nav, about

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -9,19 +9,20 @@ export default function Nav() {
   return (
     <>
       <nav >
+        {/* <img src="/images/localHarvestLogo.png" alt="Local Harvest Logo" className='lhlogo' /> */}
         <div className='navigation'>
-        <NavLink to = '/'>Home</NavLink>
-        <NavLink to = '/about'>About</NavLink>
-        <NavLink to = '/market'>Market</NavLink>
-        <NavLink to = '/involvement'>Get Involved</NavLink>
-        <NavLink to = '/bag'>Bag</NavLink>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/market'>Market</NavLink>
+          <NavLink to='/involvement'>Get Involved</NavLink>
+          <NavLink to='/bag'>Bag</NavLink>
         </div>
         <ul>
           <br />
         </ul>
         <div className='auth'>
-        <NavLink to = '/auth'>Sign Up</NavLink>
-        <NavLink to = '/auth'>Login</NavLink>
+          <NavLink to='/auth'>Become A Member</NavLink>
+          <NavLink to='/auth'>Login</NavLink>
         </div>
       </nav>
 
