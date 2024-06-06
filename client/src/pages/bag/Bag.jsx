1235d090b5f90f6d8b635b3388ac3af4b4481308
Reynolds Addy <reynoldsfrimpong@gmail.com>
2024-06-06 10:29:35 -0500

    modified css

--- client/src/pages/bag/Bag.jsx
+++ client/src/pages/bag/Bag.jsx
@@ -5,8 +5,9 @@ import './Bag.css';
 export default function Bag(){
 
 return(
-<>
- 
-</>
+<div className='bag-container'>
+  <h3 className='text'>Here are your items</h3>
+  <div className='watermark'>Watermark Text</div>
+</div>
 )
 }
