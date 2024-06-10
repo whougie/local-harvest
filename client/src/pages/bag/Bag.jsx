ea70c16dd912027f1816a5360a6e9fcd6f9f245e
JoelhansenMN <joelhansen22@gmail.com>
2024-06-10 07:32:11 -0500

    checkout button

--- client/src/pages/bag/Bag.jsx
+++ client/src/pages/bag/Bag.jsx
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react';
-// import { useShoppingCart } from "../../providers/ShoppingCartContext"
 import './Bag.css';
-
 
 const Bag = () => {
   const [price, setPrice] = useState(0);
+  const [total, setTotal] = useState();
 
-const [total, setTotal] =useState()
+  const [showMessage, setShowMessage] = useState(false);
 
-function checkoutTotal (){
-  if(sessionStorage.getItem('cart')){
-    const storage =sessionStorage.getItem('cart')
-    if(storage){
-      setTotal(JSON.parse(storage))
-      console.log(storage)
+  function checkoutTotal() {
+    if (sessionStorage.getItem('cart')) {
+      const storage = sessionStorage.getItem('cart');
+      if (storage) {
+        setTotal(JSON.parse(storage));
+      }
     }
   }
-}
-    
+
   const handlePrice = () => {
     let ans = 0;
     total && total.forEach((item) => {
@@ -34,17 +32,22 @@ function checkoutTotal (){
     const arr = total.filter((item, index) => index !== i);
     setTotal(arr);
     // Update local storage after removing the item
-  sessionStorage.setItem('cart', JSON.stringify(arr));
+    sessionStorage.setItem('cart', JSON.stringify(arr));
+  };
+
+  const handleOrderPlacement = () => {
+    // Logic to place the order here
+    setShowMessage(true);
   };
 
   useEffect(() => {
-    checkoutTotal()
-  }, [])
+    checkoutTotal();
+  }, []);
 
   return (
     <article className='checkout'>
       {total && total.map((item, i) => (
-        <div className="bag_box"  key={i}>
+        <div className="bag_box" key={i}>
           <div className="cart_img">
             {/* <img src={item.image} alt={item.title} /> */}
             <p>{item.title}</p>
@@ -59,10 +62,13 @@ function checkoutTotal (){
       ))}
       <div className='total'>
         <span>Total Price of your Bag</span>
-        <span> $  {price}</span>
+        <span> $ {price}</span>
+        <button className='checkoutBtn' type="submit" onClick={handleOrderPlacement}>Place Your Order</button>
+        {showMessage && <p>Your order has been placed successfully!</p>}
       </div>
     </article>
   );
 };
+
 
 export default Bag;
